6dc31fec64401e3f892d3276b50873469830a868
debora-smb <debora.smborges@gmail.com>
2024-05-19 17:17:10 +0200

    feat: :lipstick: add background style component

---
+++ src/app/components/atoms/GrainyBg.jsx
@@ -0,0 +1,9 @@
+import styles from './GrainyBg.module.css';
+
+export default function GrainyBg() {
+  return (
+    <div className={`${styles.background} w-full h-[100vh] border-[12px] border-black`}>
+    </div>
+
+  )
+}
