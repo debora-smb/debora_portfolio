16092e890edd26e316fa9d82954e8e996d62798e
debora-smb <debora.smb@gmail.com>
2024-08-16 17:34:23 +0200

    style: :lipstick: update header font family

--- src/app/components/atoms/Header.jsx
+++ src/app/components/atoms/Header.jsx
@@ -4,7 +4,7 @@ export default function Header(){
 
   return (
     <header className={` bg-black h-[100vh] w-[10vw] flex items-center justify-center fixed right-0`}>
-        <nav className={`text-white flex items-center justify-center flex-row gap-10 ${styles.nav}`}>
+        <nav className={`text-white flex items-center font-avenir  justify-center flex-row gap-10 ${styles.nav}`}>
             <a href="/#about-me">ABOUT ME</a>
             <a href="/#projects">PROJECTS</a>
             <a href="/#skills">SKILLS</a>
