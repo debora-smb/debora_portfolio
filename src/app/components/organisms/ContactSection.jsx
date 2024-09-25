ebea2898bc65d2648ce5da7bbc3ed2c310d233c8
debora-smb <debora.smb@gmail.com>
2024-09-25 10:28:22 +0200

    perf: :fire: Remove lineup animation of contact section title

--- src/app/components/organisms/ContactSection.jsx
+++ src/app/components/organisms/ContactSection.jsx
@@ -15,7 +15,7 @@ export default function ContactSection() {
         className="innerContainer pr-[5vw] flex flex-col lg:flex-row h-[90vh] md:h-auto lg:h-[100vh]  items-center gap-16 lg:gap-20 lg:justify-between "
       >
         <div className="text-white text-[60px] md:text-[110px] xl:text-[180px] leading-[70px] md:leading-[110px] xl:leading-[200px] lg:w-[45%]">
-          <h2 className={`${styles.lineUp} font-avenir`} >SAY <span className="font-ivyPresto">HOLA</span></h2>
+          <h2 className={`font-avenir`} >SAY <span className="font-ivyPresto">HOLA</span></h2>
         </div>
         <aside className=" w-full lg:w-[55%] flex flex-col  gap-22 lg:justify-center  justify-between h-full">
           <Form />
