b427f4a65543bfb61d2834aef071f0910e4bb2f1
debora-smb <debora.smb@gmail.com>
2024-09-25 10:32:40 +0200

    feat: :sparkles: add contact section title scroll animation

--- src/app/components/organisms/ContactSection.jsx
+++ src/app/components/organisms/ContactSection.jsx
@@ -2,24 +2,75 @@
 import Form from "../molecules/Form";
 import styles from "./Organisms.module.css";
 import Image from "next/image";
-
+import { motion } from 'framer-motion';
+import { useEffect, useState, useRef } from "react";
 
 export default function ContactSection() {
-  return (
+  const [showTitle, setShowTitle] = useState(false);
+  const [scrollDirection, setScrollDirection] = useState(null);
+  const titleRef = useRef(null); 
 
-    <div
-      className="bg-black w-[100vw] lg:w-[90vw]"
-      id="contact"
-    >
-      <div
-        className="innerContainer pr-[5vw] flex flex-col lg:flex-row h-[90vh] md:h-auto lg:h-[100vh]  items-center gap-16 lg:gap-20 lg:justify-between "
-      >
+  useEffect(() => {
+    let lastScrollY = window.scrollY;
+
+    const updateScrollDirection = () => {
+      const scrollY = window.scrollY;
+      if (scrollY > lastScrollY) {
+        setScrollDirection("down");
+      } else if (scrollY < lastScrollY) {
+        setScrollDirection("up");
+      }
+      lastScrollY = scrollY;
+    };
+
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting && scrollDirection === "down") {
+          setShowTitle(true);
+        } else if (scrollDirection === "up") {
+          setShowTitle(false);
+        }
+      },
+      { threshold: 0.2 }
+    );
+
+    if (titleRef.current) {
+      observer.observe(titleRef.current); 
+    }
+
+    window.addEventListener("scroll", updateScrollDirection); 
+
+    return () => {
+      if (titleRef.current) {
+        observer.unobserve(titleRef.current); 
+      }
+      window.removeEventListener("scroll", updateScrollDirection); 
+    };
+  }, [scrollDirection]);
+
+  return (
+    <div className="bg-black w-[100vw] lg:w-[90vw]" id="contact">
+      <div className="innerContainer pr-[5vw] flex flex-col lg:flex-row h-[90vh] md:h-auto lg:h-[100vh]  items-center gap-16 lg:gap-20 lg:justify-between">
         <div className="text-white text-[60px] md:text-[110px] xl:text-[180px] leading-[70px] md:leading-[110px] xl:leading-[200px] lg:w-[45%]">
-          <h2 className={`font-avenir`} >SAY <span className="font-ivyPresto">HOLA</span></h2>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={showTitle ? "animate" : "initial"}
+            variants={{
+              initial: { opacity: 0 },
+              animate: { opacity: 1, transition: { duration: 2, ease: "easeInOut" } },
+              exit: { opacity: 0, transition: { duration: 1, ease: "easeInOut" } }
+            }}
+            ref={titleRef} 
+          >
+            <h2 className={`${styles.lineUp} font-avenir`}>
+              SAY <span className="font-ivyPresto">HOLA</span>
+            </h2>
+          </motion.div>
         </div>
-        <aside className=" w-full lg:w-[55%] flex flex-col  gap-22 lg:justify-center  justify-between h-full">
+        <aside className="w-full lg:w-[55%] flex flex-col gap-22 lg:justify-center justify-between h-full">
           <Form />
-          <div className="flex flex-row items-center justify-between md:justify-end gap-10 ">
+          <div className="flex flex-row items-center justify-between md:justify-end gap-10">
             <Image
               height={110}
               width={80}
