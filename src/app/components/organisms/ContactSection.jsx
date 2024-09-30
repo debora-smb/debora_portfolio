a6bd1002d705a7d8c7f4a49dfc8a8207d56aa5d5
debora-smb <debora.smb@gmail.com>
2024-09-30 13:29:36 +0200

    fix: :bug: fix fade in fade out contact title animation

--- src/app/components/organisms/ContactSection.jsx
+++ src/app/components/organisms/ContactSection.jsx
@@ -7,97 +7,84 @@ import { useEffect, useState, useRef } from "react";
 import Footer from "../atoms/Footer";
 
 export default function ContactSection() {
-  const [showTitle, setShowTitle] = useState(false);
-  const [scrollDirection, setScrollDirection] = useState(null);
+  const [showTitle, setShowTitle] = useState(false); // Estado para controlar la visibilidad
   const titleRef = useRef(null); 
 
   useEffect(() => {
-    let lastScrollY = window.scrollY;
-
-    const updateScrollDirection = () => {
-      const scrollY = window.scrollY;
-      if (scrollY > lastScrollY) {
-        setScrollDirection("down");
-      } else if (scrollY < lastScrollY) {
-        setScrollDirection("up");
-      }
-      lastScrollY = scrollY;
-    };
-
-
+    // Configuración del IntersectionObserver
     const observer = new IntersectionObserver(
       ([entry]) => {
-        if (entry.isIntersecting && scrollDirection === "down") {
-          setShowTitle(true);
-        } else if (scrollDirection === "up") {
-          setShowTitle(false);
+        // Si el componente está completamente dentro del viewport (isIntersecting && intersectionRatio == 1)
+        if (entry.isIntersecting && entry.intersectionRatio === 1) {
+          setShowTitle(true); // Mostrar el título
+        } else {
+          setShowTitle(false); // Ocultar el título
         }
       },
-      { threshold: 0.2 }
+      {
+        threshold: 1 // El componente debe estar completamente dentro del viewport para que el threshold se cumpla
+      }
     );
 
     if (titleRef.current) {
-      observer.observe(titleRef.current); 
+      observer.observe(titleRef.current); // Iniciar observación del título
     }
-
-    window.addEventListener("scroll", updateScrollDirection); 
 
     return () => {
       if (titleRef.current) {
-        observer.unobserve(titleRef.current); 
+        observer.unobserve(titleRef.current); // Detener la observación cuando el componente se desmonta
       }
-      window.removeEventListener("scroll", updateScrollDirection); 
     };
-  }, [scrollDirection]);
+  }, []);
 
   return (
     <div className="bg-black w-[100vw] lg:w-[90vw]" id="contact">
       <div className="innerContainer bg-black pr-[5vw] flex flex-col h-[100vh] items-center justify-center gap-16 md:h-auto lg:h-[100vh] lg:gap-20">
-      <div className="flex flex-col  items-center gap-16 lg:flex-row lg:gap-20 lg:justify-between">
-        <div className="text-white text-[60px] leading-[70px] md:text-[110px] md:leading-[110px] lg:w-[45%] xl:text-[180px]  xl:leading-[200px] ">
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={showTitle ? "animate" : "initial"}
-            variants={{
-              initial: { opacity: 0 },
-              animate: { opacity: 1, transition: { duration: 2, ease: "easeInOut" } },
-              exit: { opacity: 0, transition: { duration: 1, ease: "easeInOut" } }
-            }}
-            ref={titleRef} 
-          >
-            <h2 className={`${styles.lineUp} font-avenir`}>
-              SAY <span className="font-ivyPresto">HOLA</span>
-            </h2>
-          </motion.div>
-        </div>
-        <aside className="w-full  flex flex-col gap-22  justify-between h-full lg:w-[55%] lg:justify-center">
-          <Form />
-          <div className="flex flex-row items-center justify-between gap-10 md:justify-end">
-            <Image
-              height={110}
-              width={80}
-              src="/img/rubber-duck.webp"
-              alt="3d Rubber duck"
-              className={`${styles.duck} opacity-50`}
-            />
-            <Image
-              height={110}
-              width={80}
-              src="/img/rubber-duck.webp"
-              alt="3d Rubber duck"
-              className={`${styles.duck} opacity-50`}
-            />
-            <Image
-              height={110}
-              width={80}
-              src="/img/rubber-duck.webp"
-              alt="3d Rubber duck"
-              className={`${styles.duck} opacity-50`}
-            />
+        <div className="flex flex-col items-center gap-16 lg:flex-row lg:gap-20 lg:justify-between">
+          <div className="text-white text-[60px] leading-[70px] md:text-[110px] md:leading-[110px] lg:w-[45%] xl:text-[180px] xl:leading-[200px] ">
+            <motion.div
+              initial={{ opacity: 0 }} // Estado inicial del título (invisible)
+              animate={showTitle ? "animate" : "initial"} // Animación basada en la visibilidad
+              variants={{
+                initial: { opacity: 0 },
+                animate: { opacity: 1, transition: { duration: 2, ease: "easeInOut" } }, // Fade in cuando está visible
+                exit: { opacity: 0, transition: { duration: 1, ease: "easeInOut" } } // Fade out cuando no es visible
+              }}
+              ref={titleRef}
+            >
+              <h2 className={`${styles.lineUp} font-avenir`}>
+                SAY <span className="font-ivyPresto">HOLA</span>
+              </h2>
+            </motion.div>
           </div>
-        </aside>
+          <aside className="w-full flex flex-col gap-22 justify-between h-full lg:w-[55%] lg:justify-center">
+            <Form />
+            <div className="flex flex-row items-center justify-between gap-10 md:justify-end">
+              <Image
+                height={110}
+                width={80}
+                src="/img/rubber-duck.webp"
+                alt="3d Rubber duck"
+                className={`${styles.duck} opacity-50`}
+              />
+              <Image
+                height={110}
+                width={80}
+                src="/img/rubber-duck.webp"
+                alt="3d Rubber duck"
+                className={`${styles.duck} opacity-50`}
+              />
+              <Image
+                height={110}
+                width={80}
+                src="/img/rubber-duck.webp"
+                alt="3d Rubber duck"
+                className={`${styles.duck} opacity-50`}
+              />
+            </div>
+          </aside>
         </div>
-            <Footer />
+        <Footer />
       </div>
     </div>
   );
