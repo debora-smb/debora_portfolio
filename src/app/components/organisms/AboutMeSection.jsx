4d2a4553fde6079d86fe1bffb84ab4d331d8daf8
debora-smb <debora.smb@gmail.com>
2024-09-30 15:52:39 +0200

    feat: :sparkles: reestructure follow me component code and add cv file

--- src/app/components/organisms/AboutMeSection.jsx
+++ src/app/components/organisms/AboutMeSection.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./Organisms.module.css";
 import InfoAboutMe from "../molecules/InfoAboutMe";
+import FollowMe from "../molecules/FollowMe";
 
 export default function AboutMeSection() {
 
@@ -9,34 +10,8 @@ export default function AboutMeSection() {
             <div className={`innerContainerNoPadding gap-4 h-[100vh] justify-center md:gap-12 ${styles.aboutContainer}`}>
                 <div className="text-white text-[26px] leading-[30px] flex flex-col gap-4 justify-center relative z-2 lg:gap-20 ">
                     <InfoAboutMe />
-                    <div
-                        className={`font-ivyPresto text-[30px] mt-14 leading-[50px] text-white flex-row items-center gap-8 h-12 hidden lg:mt-0 lg:flex`}
-                    >
-                        follow me
-                        <a
-                            href="https://github.com/debora-smb"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <Image
-                                width={25}
-                                height={25}
-                                src="/img/icon-github.svg"
-                                alt="GitHub"
-                            />
-                        </a>
-                        <a
-                            href="https://www.linkedin.com/in/debora-smenezes"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <Image
-                                width={25}
-                                height={25}
-                                src="/img/icon-linkedin.svg"
-                                alt="LinkedIn"
-                            />
-                        </a>
+                    <div className="hidden lg:flex">
+                        <FollowMe />
                     </div>
                 </div>
                 <aside className="bg-yellow h-fit">
@@ -48,34 +23,8 @@ export default function AboutMeSection() {
                         className="h-[300px] w-full object-cover md:h-[500px] lg:h-[90vh]"
                     />
                 </aside>
-                <div
-                    className={`font-ivyPresto text-[14px]  leading-[12px] text-white flex flex-row items-center gap-8 md:text-[24px] lg:hidden`}
-                >
-                    follow me
-                    <a
-                        href="https://github.com/debora-smb"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <Image
-                            width={25}
-                            height={25}
-                            src="/img/icon-github.svg"
-                            alt="GitHub"
-                        />
-                    </a>
-                    <a
-                        href="https://www.linkedin.com/in/debora-smenezes"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <Image
-                            width={25}
-                            height={25}
-                            src="/img/icon-linkedin.svg"
-                            alt="LinkedIn"
-                        />
-                    </a>
+                <div className="flex lg:hidden">
+                    <FollowMe />
                 </div>
             </div>
         </div>
