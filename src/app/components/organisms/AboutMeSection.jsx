c072eb620c80d36ca3599975a40c01a40ba64c7a
debora-smb <debora.smb@gmail.com>
2024-10-10 18:02:13 +0200

    style: :lipstick: update about me section style

--- src/app/components/organisms/AboutMeSection.jsx
+++ src/app/components/organisms/AboutMeSection.jsx
@@ -7,7 +7,7 @@ export default function AboutMeSection() {
 
     return (
         <div className="bg-black w-[100vw] h-[100vh] lg:pr-0 lg:w-[90vw]" id="about-me">
-            <div className={`innerContainer gap-4 h-[100vh] flex flex-col items-center md:gap-12 ${styles.aboutContainer}`}>
+            <div className={`innerContainer gap-4 h-[100vh] flex flex-col justify-center items-center md:gap-12 ${styles.aboutContainer}`}>
                 <div className="text-white text-[26px] w-full leading-[30px] flex flex-col gap-4 lg:gap-20 ">
                     <InfoAboutMe />
                     <div className="hidden lg:flex">
